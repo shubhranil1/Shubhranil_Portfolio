32fda50ce526e6748c41fb02811f0588c6ab4f36
SHUBHRANIL CHOWDHURY <subhranilchowdhury27@gmail.com>
2026-01-18 15:26:24 +0530

    Replace BrowserRouter with HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Shubhranil_Portfolio">
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-)
+  </HashRouter>
+);
