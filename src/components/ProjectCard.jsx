1314f7b4da38337d0037c59e167116797fc017e9
SHUBHRANIL CHOWDHURY <subhranilchowdhury27@gmail.com>
2026-01-22 22:16:48 +0530

    Enhance title styling in ProjectCard component for improved readability

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({title,description,image,tech}) => {
     <div className='bg-dark-300 rounded-2xl overflow-hidden hover:-translate-y-2 transition duration-300 cursor-pointer'>
         <img src={image} alt={title} className='w-full h-60 object-center object-fill '/>
         <div className='p-6 space-y-3'>
-            <h3>{title}</h3>
+            <h3 className='text-3xl font-semibold'>{title}</h3>
             <p>{description}</p>
             <div className='flex items-center gap-2 flex-wrap text-sm text-gray-400'>
                 {tech.map((item,index) => (
