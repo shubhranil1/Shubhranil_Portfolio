e02fa0ae0ebaef3094b956d57d24a5a3b026cd0f
SHUBHRANIL CHOWDHURY <subhranilchowdhury27@gmail.com>
2026-01-22 23:16:31 +0530

    Refactor ProjectCard component for improved layout and styling consistency

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,14 +6,23 @@ const ProjectCard = ({title,description,image,tech}) => {
     <div className='bg-dark-300 rounded-2xl overflow-hidden hover:-translate-y-2 transition duration-300 cursor-pointer'>
         <img src={image} alt={title} className='w-full h-60 object-center object-fill '/>
         <div className='p-6 space-y-3'>
-            <h3 className='text-3xl font-semibold'>{title}</h3>
-            <p>{description}</p>
-            <div className='flex items-center gap-2 flex-wrap text-sm text-gray-400'>
+            <h3 className='text-2xl font-semibold mb-2'>{title}</h3>
+            <p className='text-gray-400'mb-4>{description}</p>
+            <div className='flex flex-wrap gap-2 mb-4'>
                 {tech.map((item,index) => (
-                    <span key={index} className=''>
+                    <span  key={index} className='px-3 py-1 bg-dark-400 rounded-full text-sm '>
                         {item}
                     </span>
                 ))}
+            </div>
+            <div className='flex-gap-2  '>
+                <a href="#" className='flex-1 text-center px-4 py-2 bg-purple rounded-lg font-medium hover:bg-purple-700 transition duration-300 '>
+                    live demo
+                </a>
+                <a href="#" className='flex-1 text-center px-4 py-2 m-3 border  border-purple font-medium rounded-lg hover:bg-purple/20 transition duration-300'>
+                    Code
+                </a>
+
             </div>
 
 
